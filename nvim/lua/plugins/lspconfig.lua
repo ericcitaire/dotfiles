a36d2ae7b13e1532d32033b65c9014398608017d
Eric Citaire <e.citaire@media-participations.com>
2026-06-10 21:48:54 +0000

    more config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,8 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
+            "mason-org/mason-lspconfig.nvim",
+            "mason-org/mason.nvim",
 		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
