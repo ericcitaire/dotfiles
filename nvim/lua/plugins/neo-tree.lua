d43f7a683e13de956714f022c09c229d6813fdbb
Eric Citaire <e.citaire@media-participations.com>
2026-07-01 07:42:20 +0000

    config(nvim): show hidden files

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,26 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = {
+                    '.git',
+                    '.DS_Store',
+                    'thumbs.db',
+                },
+                never_show = {},
+            },
         },
-        lazy = false, -- neo-tree will lazily load itself
-        conf = {
-            close_if_last_window = true,
-        },
-    }
+    },
 }
