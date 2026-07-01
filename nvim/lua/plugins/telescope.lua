10ee85fbd9b44f39410d82650c02c6800b127dbd
Eric Citaire <e.citaire@media-participations.com>
2026-07-01 07:44:55 +0000

    config(nvim): configure rg

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,6 +5,27 @@ return {
 			'nvim-lua/plenary.nvim',
 			-- optional but recommended
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-		}
+		},
+		opts = {
+			defaults = {
+				vimgrep_arguments = {
+					'rg',
+					'--color=never',
+					'--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'--hidden',
+					'--glob',
+					'!**/.git/*',
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		},
 	}
 }
